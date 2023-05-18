4c8cf32c1c1beb833714e68936ab00d489eb5525
dcha7225 <danielcha0131@gmail.com>
2023-05-18 12:00:40 -0400

    setup menu tab

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.background}>
+      <Text>Hello man</Text>
+
+      <View style={styles.menubar}>
+      <View style={styles.menuBarOptions}>
+        <Image source={require('./assets/graphIcon.png')} style={{
+        height: "60%",
+        width: "43%"
+        }}/>
+        <Text>Charts</Text>
+      </View>
+      <View style={styles.menuBarOptions}>
+        <Image source={require('./assets/todayIcon.png')} style={{
+          height: "60%",
+          width: "50%"
+          }}/>
+          <Text>Today</Text>
+      </View>
+       <View style={styles.menuBarOptions}>
+         <Image source={require('./assets/journalIcon.png')} style={{
+          height: "60%",
+          width: "60%"
+          }}/>
+        <Text> Journal </Text>
+        </View>
+      </View>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center'
   },
+
+  menubar: {
+    width:'100%',
+    height: '13%',
+    bottom: 0,
+    position: 'absolute',
+    flexDirection: 'row',
+    borderWidth: 2,
+    borderColor: 'grey',
+    borderRadius: 15,
+    overflow: 'hidden'
+  },
+  menuBarOptions:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
 });
